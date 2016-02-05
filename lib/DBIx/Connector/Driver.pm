5b01cc19fb4c50ab81420ffad84ef5fbc467b1b3
David E. Wheeler <david@justatheory.com>
2016-02-05 18:59:50 +0000

    Import of DWHEELER/DBIx-Connector-0.55.tar.gz from CPAN.
    gitpan-cpan-distribution: DBIx-Connector
    gitpan-cpan-version:      0.55
    gitpan-cpan-path:         DWHEELER/DBIx-Connector-0.55.tar.gz
    gitpan-cpan-author:       DWHEELER
    gitpan-cpan-maturity:     released

--- lib/DBIx/Connector/Driver.pm
+++ lib/DBIx/Connector/Driver.pm
@@ -2,7 +2,7 @@ package DBIx::Connector::Driver;
 
 use strict;
 use warnings;
-our $VERSION = '0.54';
+our $VERSION = '0.55';
 
 DRIVERS: {
     my %DRIVERS;
@@ -80,6 +80,7 @@ sub rollback_to {
 
 ROLLBACKERR: {
     package DBIx::Connector::RollbackError;
+    our $VERSION = '0.55';
     # an exception is always true
     use overload bool => sub {1}, '""' => 'as_string', fallback => 1;
 
@@ -94,10 +95,12 @@ ROLLBACKERR: {
     }
 
     package DBIx::Connector::TxnRollbackError;
+    our $VERSION = '0.55';
     our @ISA = ('DBIx::Connector::RollbackError');
     sub _label    { 'Transaction' }
 
     package DBIx::Connector::SvpRollbackError;
+    our $VERSION = '0.55';
     our @ISA = ('DBIx::Connector::RollbackError');
     sub _label    { 'Savepoint' }
 }
